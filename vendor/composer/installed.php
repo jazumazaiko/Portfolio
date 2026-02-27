edc3f632064843f03fd8930f205b4672804ee420
Sygnorey Jazze Azuma <sygnoreyjazze@gmail.com>
2026-02-27 22:18:16 +0800

    Portfolio-1v

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'npm-asset/bootstrap' => array(
+            'pretty_version' => '5.3.8',
+            'version' => '5.3.8.0',
+            'reference' => null,
+            'type' => 'npm-asset',
+            'install_path' => __DIR__ . '/../npm-asset/bootstrap',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'npm-asset/bootstrap-icons' => array(
+            'pretty_version' => '1.13.1',
+            'version' => '1.13.1.0',
+            'reference' => null,
+            'type' => 'npm-asset',
+            'install_path' => __DIR__ . '/../npm-asset/bootstrap-icons',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
